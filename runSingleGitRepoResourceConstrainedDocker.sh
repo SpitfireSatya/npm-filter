2cb9f86a93955b34e7a41ba9700aa38898753919
faridekhalili <faridekhalili1393@gmail.com>
2023-03-28 21:10:03 -0400

    Fix runSingleGitRepoResourceConstrainedDocker.sh.

--- runSingleGitRepoResourceConstrainedDocker.sh
+++ runSingleGitRepoResourceConstrainedDocker.sh
@@ -13,13 +13,13 @@ fi
 SOURCE="`pwd`/local_mount_$RANDOM"
 
 
-docker run --mount type=bind,source="$SOURCE",destination=/mount \
+docker run -v type=bind,source="$SOURCE",destination=/mount \
                    --volume `pwd`/npm_filter_parallel_docker_results:/home/npm-filter/results \
                    --cpus="$CPUS" \
                    --memory="${RAM}g" \
                    -w /home/npm-filter \
                    --rm \
-                   -it npm-filter:latest \
+                   emarteca/npm-filter:latest \
                    bash -c "python3 src/diagnose_github_repo.py --repo_link $repo_link --config results/config.json --output_dir results"
 
 rm -r "local_mount_$RANDOM"
